b73a319eee188a55dd20d964bc0cdee8d39d93c5
BashirHam <bachirawsome@gmail.com>
2025-10-03 09:41:02 +0100

    Update XO.jsx

--- XO.jsx
+++ XO.jsx
@@ -1,4 +1,6 @@
-import react, { useState } from 'react'
+
+
+import react, { useRef, useState } from 'react'
 import './XO.css'
 import circle_icon from '../src/components/assets/circle.png'
 import cross_icon from '../src/components/assets/cross.png'
@@ -10,6 +12,7 @@ const XO = () => {
 
     let[count,setCount] = useState(0);
     let [lock,setlock] = useState(false);
+    let titleref = useRef(null)
 
     const toggle = (w,num) => {
       if (lock) {
@@ -26,11 +29,56 @@ const XO = () => {
        data[num]="o";
        setCount(++count)
     }
+    checkwin()
     }
+    const checkwin = () => {
+        if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+    (won[data])}
+}
+    else  if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
+    {
+        won(data[2])
+    }
+    else  if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
+    {
+        won(data[5])
+    }
+    else  if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
+    {
+        won(data[8])
+    }
+    else  if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
+    {
+        won(data[6])
+    }
+    else  if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
+    {
+        won(data[7])
+    }
+    else  if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+    {
+        won(data[8])
+    }
+    else  if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
+    {
+        won(data[8])
+    }
+ {}
+ const won = (winner) => {
+    setlock(true)
+    if(winner==="x")
+    {
+        titleref.current.innerHTML = `congratulations : <img src=${cross_icon}>`
+    }
+
+    else{
+        titleref.current.innerHTML = `congratulations : <img src=${circle_icon}>`
+    }
+ 
     return (
         <div>
            <div className='container'>
-      <h1 className="title">X O Game In <span>react</span></h1>
+      <h1 className="title" ref={titleref}>X O Game In <span>react</span></h1>
     <div className="board">
         <div className="row1">
             <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
