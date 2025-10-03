b6a22c56be73c4d72221ef53e6fb78a10dea52c9
BashirHam <bachirawsome@gmail.com>
2025-10-03 03:47:32 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import XO from './XO/XO'
+ReactDOM.createroot(document.getElementById('root')).render(<app />);
+
+function app() {
+  return
+   
+  
+}
+
+export default App;
---
+++ XO.jsx
@@ -0,0 +1,58 @@
+import react, { useState } from 'react'
+import './XO.css'
+import circle_icon from '../src/components/assets/circle.png'
+import cross_icon from '../src/components/assets/cross.png'
+import App from '../App'
+
+let data = ["","","","","","",""]
+
+const XO = () => {
+
+    let[count,setCount] = useState(0);
+    let [lock,setlock] = useState(false);
+
+    const toggle = (w,num) => {
+      if (lock) {
+        return 0;
+      }
+      if(count%2===0)
+      {
+       e.target.innerHTML = `<img src='${cross_icon}'>`;
+       data[num]="x";
+       setCount(++count)
+    }
+    else{
+         e.target.innerHTML = `<img src='${circle_icon}'>`;
+       data[num]="o";
+       setCount(++count)
+    }
+    }
+    return (
+        <div>
+           <div className='container'>
+      <h1 className="title">X O Game In <span>react</span></h1>
+    <div className="board">
+        <div className="row1">
+            <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
+        </div>
+        <div className="row2">
+            <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
+        </div>
+        <div className="row3">
+            <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
+            <div className="boxes"> onClick={(e)=>{toggle(e,8)}}</div>
+        </div>
+
+    </div>
+    <button className="reset">Reset</button>
+    </div>
+        </div>
+    )
+}
+
+export default XO;
